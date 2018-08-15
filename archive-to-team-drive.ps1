bccfce14875af08926a684ce07e9ea0a1440790d
Caelan <caelan.borowiec@pqi.org>
2018-08-15 17:07:25 -0400

    New logic to not archive subdirs of dirs with files

--- archive-to-team-drive.ps1
+++ archive-to-team-drive.ps1
@@ -2,7 +2,7 @@ $source="G:\My Drive\Test data" #Data to back up
 $target="G:\Team Drives\Hot Swap\ArchiveTest" #Backup root
 
 # Select all directories that contain 1 or more files:
-$dirs = Get-ChildItem -Path $source -Recurse | where {$_.psiscontainer -AND (gci -File $_.fullName).count -ne 0 }
+$dirs = Get-ChildItem -Path $source -Recurse | where {$_.psiscontainer -AND (Get-ChildItem -File $_.fullName).count -ne 0 }
 
 Foreach ($dir in $dirs)
 {
@@ -10,12 +10,18 @@ Foreach ($dir in $dirs)
       $oldpath = $dir.FullName
       $newpath = ($oldpath -replace [regex]::Escape($source), $target)
 
-      Write-Host -NoNewline $oldpath " -> " $newpath `r`n
+      #Write-Host -NoNewline $oldpath " -> " $newpath `r`n
 
       $newname = $name.replace(" ","_")
 
       $archive = $newpath + ".zip" #Add the rest of the path here
 
       New-Item -ItemType Directory -Force -Path (Split-Path -Path $newpath -Parent)
-      Compress-Archive -Path $oldpath -Update -DestinationPath $archive
+      #Compress-Archive -Path $oldpath -Update -DestinationPath $archive
+
+      $files = Get-ChildItem -File -Path $oldpath
+      Foreach ($file in $files)
+      {
+        Compress-Archive -Path $file.FullName -Update -DestinationPath $archive
+      }
 }
