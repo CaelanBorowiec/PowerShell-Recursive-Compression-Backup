345a199f094506b1a6654dc5cf9ce29d8cbe0062
Caelan <caelan.borowiec@pqi.org>
2018-08-15 14:46:27 -0400

    Starter code
    Grabbed from
    https://community.spiceworks.com/topic/780460-zip-folders-via-powershell

---
+++ archive-to-team-drive.ps1
@@ -0,0 +1,15 @@
+# Borrowed this piece. Tests for 7Zip and if it exists creates an alias
+if (-not (test-path "$env:ProgramFiles\7-Zip\7z.exe")) {throw "$env:ProgramFiles\7-Zip\7z.exe needed"}
+set-alias sz "$env:ProgramFiles\7-Zip\7z.exe"
+$files="c:\backup"
+$target="c:\archive"
+$dirs = Get-ChildItem -Path $files | Where-Object { $_.Attributes -eq "Directory" }
+
+# Creates 7z files based on the parent folders name and drops it inside the archive folder
+Foreach ($dir in $dirs)
+{
+      $name = $dir.name
+      $newname = $name.ToLower() -replace(" ","")
+      sz a -t7z "$target\$newname" "$files\$dir"
+
+}
