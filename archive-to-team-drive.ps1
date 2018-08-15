2aa522bc647a11d0f7f04ede9981c879f7c23841
Caelan <caelan.borowiec@pqi.org>
2018-08-15 14:46:40 -0400

    Change paths

--- archive-to-team-drive.ps1
+++ archive-to-team-drive.ps1
@@ -1,8 +1,8 @@
 # Borrowed this piece. Tests for 7Zip and if it exists creates an alias
 if (-not (test-path "$env:ProgramFiles\7-Zip\7z.exe")) {throw "$env:ProgramFiles\7-Zip\7z.exe needed"}
 set-alias sz "$env:ProgramFiles\7-Zip\7z.exe"
-$files="c:\backup"
-$target="c:\archive"
+$files="G:\My Drive\Test data"
+$target="G:\Team Drives\Hot Swap\ArchiveTest"
 $dirs = Get-ChildItem -Path $files | Where-Object { $_.Attributes -eq "Directory" }
 
 # Creates 7z files based on the parent folders name and drops it inside the archive folder
